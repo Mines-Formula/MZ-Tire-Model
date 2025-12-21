42c4885dce2d469ca85897aa6f393162e0167e4b
PMurphyB <pmurphyguitar@gmail.com>
2025-12-21 11:42:39 -0600

    Multiple Data Updates

--- generator.m
+++ generator.m
@@ -7,7 +7,7 @@ idxRange50 = 23698:24905;
 idxRange100 = 26192:27334;
 idxRange150 = 22439:23646;
 idxRange200 = 21195:22363;
-idxRange250a = 24933:26158;
+idxRange250a = 25379:25829;
 %idxRange250b = 19947:21171;
 
 curFile = load(dataFile);

1a288a8c04fa0343c81b1d5f5b4e5e20cddcd6b1
PMurphyB <pmurphyguitar@gmail.com>
2025-12-23 08:42:41 -0600

    Added A Bunch Of Comments
    I wanted to comment in each of the sections so that it's easier to keep track of what section is doing what.  You can find what each of these are referencing in the textbook.

--- pacejka_MZ.m
+++ pacejka_MZ.m
@@ -1,5 +1,6 @@
 function [Mz_pred, Kza0, Kzg0] = pacejka_MZ(P, Fz, alpha, gamma, Fy0, Kya0, R0)
 
+%Parameters
 Bt = P(1);
 Ct = P(2);
 Dt1 = P(3);
@@ -16,26 +17,35 @@ qHz4 = P(13);
 qDz8 = P(14);
 qDz9 = P(15);
 
-df = Fz ./ max(Fz);
-gamma_star = gamma;
+%Normalized Load and camber
+df = Fz ./ max(Fz); % normalized load
+gamma_star = gamma; % normalized camber
 
+%4.E35
 SHt = qHz1 + qHz2 .* df.^2 + (qHz3 + qHz4 .* df.^2) .* gamma_star;
 
+%4.E37
 alpha_t alpha + SHt;
 
+%4.E33
 Dt = (Dt1 .* Fz + Dt2);
 t0 = Dt .* cos(Ct.*atan(Bt .* alpha_t - Et .* (Bt .* alpha_t - atan(Bt .* alpha_t)))) .* cos(alpha);
 
+%4.E32
 Mz0_prime = -t0 .* Fy0;
 
+%4.E36
 alpha_r = alpha;
 Dr = (Dr1 .* Fz + Dr2);
 Mzr0 = Dr .* cos(Cr .* atan(Br .* alpha_r));
 
+%4.E31
 Mz_pred = Mz0_prime + Mzr0;
 
+%4.E48
 Kza0 = Dt .* Kya0;
 
+%4.E49
 Kzg0 = Fz .* R0 .* (qDz8 + qDz9 .* df.^2);
 
 end

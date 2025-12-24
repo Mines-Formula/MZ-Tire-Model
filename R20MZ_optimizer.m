26bfb9c2c5fb512be304b3af5287b672d93d434a
PMurphyB <pmurphyguitar@gmail.com>
2025-12-24 08:31:57 -0600

    Setup Data Files in Optimizer

--- R20MZ_optimizer.m
+++ R20MZ_optimizer.m
@@ -1,2 +1,6 @@
 clc, clearvars, close all
 
+data0 = readtable('data/R20MZ_combined_filtered.csv');
+data2 = readtable('data/R20MZ_IA-2_combined_filtered.csv');
+data4 = readtable('data/R20MZ_IA-4_combined_filtered.csv');
+data  = [data0; data2; data4];

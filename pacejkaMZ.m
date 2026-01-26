0d6931617e48d817e86fae486c20adcc6d5b0f52
PMurphyB <pmurphyguitar@gmail.com>
2026-01-26 11:30:08 -0700

    Update pacejkaMZ.m

--- pacejkaMZ.m
+++ pacejkaMZ.m
@@ -1,5 +1,12 @@
 function MZ0 = pacejkaMZ(P, Q, L, FZ, IA, alpha, FY);
 
+%P: The coefficients derived from the FY optimization.
+%Q: Coefficients being optimized from the textbook
+%L: Lambdas in the textbook
+%IA: Inclination Angle
+%alpha: Slip angle
+%FY: Cornering Force input (currently not used in this build)
+
 % inputs 24 Q coefficients,
 % Estimated coefficients are as follows (found in Appen:
 %Q = [0.007, -0.002, 0.147, 0.004, 8.964, -1.106, -0.842, 0, -0.227, 1.180, 0.1, -0.001, 0.007, 13.05, -1.609, -0.359, 0, 0.174, -0.896, 0, -0.008, 0, -0.296, -0.009];
